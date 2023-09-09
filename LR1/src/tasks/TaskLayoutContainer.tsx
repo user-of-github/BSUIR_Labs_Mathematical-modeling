cd4f51fab0024354e2aecf9ed1d78a1e920737cc
Nikita <nikitosminsk100@gmail.com>
2023-09-09 17:18:49 +0300

    feat: LR1

--- LR1/src/tasks/TaskLayoutContainer.tsx
+++ LR1/src/tasks/TaskLayoutContainer.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 export const TaskLayoutContainer = ({children}: React.PropsWithChildren<object>): JSX.Element => {
     return (
-      <section className="flex flex-col justify-start border-fuchsia-200 border p-1 max-h-screen">
+      <section className="flex w-full flex-col justify-start border-fuchsia-200 border p-1 max-h-screen">
           {children}
       </section>
     );
